a1c36975862cc2aa89e8dc576be5395251e31764
Dustan.Mbaga <Dustan.Mbaga@nmbbank.co.tz>
2020-06-04 18:42:29 +0300

    Item

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 use App\Item;
 
@@ -8,28 +9,24 @@ use Illuminate\Http\Request;
 
 class ItemsController extends Controller
 {
-    public function getAllItems()
-     {
-    return DB::select('select it.id, it.item_name, ic.name as category, it.item_description, e.name as event, it.report_event_date, it.reporter_mssdn,
+    public function getAllItems() {
+        return DB::select('select it.id, it.item_name, ic.name as category, it.item_description, e.name as event, it.report_event_date, it.reporter_mssdn,
     it.reporter_email, rs.name as status, it.report_event_location, it.created_at from item it
     join item_category ic on ic.id=it.item_category_id
     join event e on e.id=it.report_event_id
     join report_status rs on rs.id=it.report_status_id');
     }
 
-    public function getItem($id)
-     {
-    return DB::select('select it.id, it.item_name, ic.name as category, it.item_description, e.name as event, it.report_event_date, it.reporter_mssdn,
+    public function getItem($id) {
+        return DB::select('select it.id, it.item_name, ic.name as category, it.item_description, e.name as event, it.report_event_date, it.reporter_mssdn,
     it.reporter_email, rs.name as status, it.report_event_location, it.created_at from item it
     join item_category ic on ic.id=it.item_category_id
     join event e on e.id=it.report_event_id
     join report_status rs on rs.id=it.report_status_id
-    where it.id='.$id);
+    where it.id=' . $id);
     }
 
-
-    public function addItem(Request $request){
-
+    public function addItem(Request $request) {
 
         $item = new Item;
         $item->item_name = $request->item_name;
@@ -42,19 +39,20 @@ class ItemsController extends Controller
         $item->report_status_id = $request->status;
         $item->report_event_location = $request->report_event_location;
 
-
         $item->save();
 
-        return response()->json([
-            "message" => "item record created"
-        ], 201);
-
-
+        return response()->json(["message" => "item record created"], 201);
     }
 
-    public function getAllIItemCategory(){return DB::select('select * from item_category');}
+    public function getAllIItemCategory() {
+        return DB::select('select * from item_category');
+    }
 
-    public function getReportStatus(){return DB::select('select * from report_status');}
+    public function getReportStatus() {
+        return DB::select('select * from report_status');
+    }
 
-    public function getEvents(){return DB::select('select * from event');}
+    public function getEvents() {
+        return DB::select('select * from event');
+    }
 }
